50dbfcfb0db23ee2245e558547cefb7eaabb56c9
Ismael Rodrigo <86257396+ismael-rodrigo@users.noreply.github.com>
2021-08-18 21:54:29 -0300

    add while

--- Code_python/code_a.py
+++ Code_python/code_a.py
@@ -19,28 +19,28 @@ arduino.baudrate = baud
 arduino.port = port
 arduino.open()
 
+while True:
+        #ARMAZENANDO ESTATISTICAS DO HARDWARE
+    info_net = psutil.net_if_addrs()
+    info_memory = psutil.virtual_memory() 
+    info_disk = psutil.disk_usage('/') 
+    boot_minutes = psutil.boot_time()
+    IP = str(info_net[placa_rede][0].address)
+    IP = IP.replace(".","")
 
-    #ARMAZENANDO ESTATISTICAS DO HARDWARE
-info_net = psutil.net_if_addrs()
-info_memory = psutil.virtual_memory() 
-info_disk = psutil.disk_usage('/') 
-boot_minutes = psutil.boot_time()/120
-    #hardware_temp = psutil.sensors_temperatures(fahrenheit=False)
-    #keylist = list(hardware_temp.keys())
-    #for d in range(len(keylist)):
-    #   print(f" {keylist[d]}  :{hardware_temp[keylist[d]][0].current}° Graus
 
-stats = {
-    "CPU_PERCENT":      psutil.cpu_percent(interval= 1) ,
-    "MEMORY_TOTAL" :    int(info_memory.total/10000000) ,
-    "MEMORY_AVAILABLE": int(info_memory.available /10000000),
-    "MEMORY_USED"  :    int(info_memory.used/10000000),
-    "HD" :              info_disk.percent,
-    "IPV4" :            info_net[placa_rede][0].address,
-    "TIME_BOOT":        boot_minutes
-    }
+    stats = {
+        "CPU_PERCENT":      int(psutil.cpu_percent(interval= 1)*10) ,
+        "MEMORY_TOTAL" :    int(info_memory.total/10000000) ,
+        "MEMORY_AVAILABLE": int(info_memory.available /10000000),
+        "MEMORY_USED"  :    int(info_memory.used/10000000),
+        "HD" :              int(info_disk.percent),
+        "IPV4" :            int(IP)          
+        }
 
-print(stats)
+    arduino.write(stats["CPU_PERCENT"])
+    print(stats["CPU_PERCENT"])
+    time.sleep(1)
 
 
 
